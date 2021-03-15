e1e1711aef968afea2b9b8a6363d296c8d5d88c1
torumitsutake <torumitsutake@gmail.com>
2021-03-15 18:38:24 +0900

    素因数分解の例を実装

--- examples/half_addar.py
+++ examples/half_addar.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import qgate
 from qgate.script import *
 
+
+
 # Glover's algorithm
 
 # allocating qubit register
@@ -11,19 +13,19 @@ qregs = new_qregs(3)
 q0, q1, q2 = qregs[0], qregs[1], qregs[2]
 
 #input
-circuit = [
-    
-]
-
-
-# building a quantum circuit.
+circuit = [I(qreg) for qreg in qregs]
 circuit += [
-    ctrl(q0,q1).X(q2),
-    ctrl(q0).X(q1)
+    X(q0)
 ]
+
+circuit += [
+ctrl(q0,q1).X(q2),
+ctrl(q0).X(q1)
+]
+
 
 # measure
-refs = new_references(2)
+refs = new_references(3)
 circuit += [
     measure(refs[0], qregs[0]),
     measure(refs[1], qregs[1]),
@@ -36,9 +38,6 @@ sim.run(circuit)
 obs = sim.obs(refs)
 print('observation\n'
       '(c0, c1, c2) = ({}, {}, {})'.format(obs(refs[0]), obs(refs[1]), obs(refs[2])))
-print(obs)
 
-print('\ndump state vector:')
-qgate.dump(sim.qubits)
 
 sim.terminate()
--- examples/prime_factorization.py
+++ examples/prime_factorization.py
@@ -4,57 +4,76 @@ from __future__ import print_function
 import qgate
 from qgate.script import *
 
-# Glover's algorithm
-
+def Tdag(qred):
+    return [
+        [T(qred) for i in range(7)]
+    ]
 # allocating qubit register
-qregs = new_qregs(4)
+qregs = new_qregs(5)
 q0, q1, q2, q3 = qregs[0], qregs[1],qregs[2], qregs[3]
 
 # building a quantum circuit.
-circuit = [
-    [H(qreg) for qreg in qregs],
-    H(q1),
-    ctrl(q0).X(q1),
-    H(q1),
-    [H(qreg) for qreg in qregs],
-    [X(qreg) for qreg in qregs],
-    H(q1),
-    ctrl(q0).X(q1),
-    H(q1),
-    [X(qreg) for qreg in qregs],
-    [H(qreg) for qreg in qregs]
-]
+
+#circuit = [
+#    [H(qreg) for qreg in qregs]
+#]
+#f(x) = 4^x(mod15)
+#4 =  0100
+#15 = 1111
 circuit = [
     H(q0),H(q1),
     ctrl(q1).X(q2),
-    X(q3),ctrl(q1).X(q3),
-    H(q0),T(q1),Z(q1),
+    X(q3),ctrl(q1).X(q3)
+    ]
+circuit += [
+    H(q0),
+    [T(q1) for i in range(7)],
     ctrl(q0).X(q1),
     T(q1),
     ctrl(q0).X(q1),
-    H(q1),T(q0),Z(q0),
+    H(q1),
+    [T(q0) for i in range(7)],
     ctrl(q0).X(q1),
     ctrl(q1).X(q0),
     ctrl(q0).X(q1)
 ]
 
-
 # measure
 refs = new_references(4)
 circuit += [
-    measure(refs[0], qregs[0]),
-    measure(refs[1], qregs[1])
+    measure(refs[0], qregs[1]),
+    measure(refs[1], qregs[0])
 ]
 
-sim = qgate.simulator.py()
-sim.run(circuit)
+count0 = 0
+count1 = 0
+count2 = 0
+count3 = 0
 
-obs = sim.obs(refs)
-print('observation\n'
-      '(c0, c1) = ({}, {})'.format(obs(refs[0]), obs(refs[1])))
-print(obs)
+for i in range(100):
+    sim = qgate.simulator.py()
+    sim.run(circuit)
+    obs = sim.obs(refs)
+    print('observation\n'
+          '(c0, c1) = ({}, {})'.format(obs(refs[0]), obs(refs[1])))
+    print(obs)
+    c0 = obs(refs[0])
+    c1 = obs(refs[1])
+    if c0 == 0 and c1 == 0:
+        count0 += 1
+    if c0 == 1 and c1 == 0:
+        count1 += 1
+    if c0 == 0 and c1 == 1:
+        count2 += 1
+    if c0 == 1 and c1 == 1:
+        count3 += 1
 
-print('\ndump state vector:')
-qgate.dump(sim.qubits)
+print("count0",count0)
+print("count1",count1)
+print("count2",count2)
+print("count3",count3)
+
+#print('\ndump state vector:')
+#qgate.dump(sim.qubits)
 
 sim.terminate()
